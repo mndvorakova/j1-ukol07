c5ae45f6d91da10389197e0ab3fe00ed1ccb6de5
maja-nastassjadvorakova <dvorakova.maja12@gmail.com>
2025-06-22 19:54:31 +0200

    třídy Kniha - přidána JavaBean  - atributy autor, nazevKnihy, rokVydani

--- src/main/java/cz/czechitas/ukol07/Kniha.java
+++ src/main/java/cz/czechitas/ukol07/Kniha.java
@@ -1,4 +1,31 @@
 package cz.czechitas.ukol07;
 
 public class Kniha {
+    private String autor;
+    private String nazevKnihy;
+    private int rokVydani;
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getNazevKnihy() {
+        return nazevKnihy;
+    }
+
+    public void setNazevKnihy(String nazevKnihy) {
+        this.nazevKnihy = nazevKnihy;
+    }
+
+    public int getRokVydani() {
+        return rokVydani;
+    }
+
+    public void setRokVydani(int rokVydani) {
+        this.rokVydani = rokVydani;
+    }
 }
