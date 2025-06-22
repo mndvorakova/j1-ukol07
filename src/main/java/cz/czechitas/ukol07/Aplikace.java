70a48a1eb23cc7c0e01437f8dbbab553b4701686
maja-nastassjadvorakova <dvorakova.maja12@gmail.com>
2025-06-22 21:25:38 +0200

    Aplikace.java - doplněny výpisy do konzole

--- src/main/java/cz/czechitas/ukol07/Aplikace.java
+++ src/main/java/cz/czechitas/ukol07/Aplikace.java
@@ -1,12 +1,26 @@
 package cz.czechitas.ukol07;
 
+import java.util.List;
+
 /**
  * Spouštěcí třída aplikace
  */
 public class Aplikace {
 
-    public static void main(String[] args) {
+    private static List<Kniha> knihy1845;
 
+    public static void main(String[] args) {
+        KnihaSluzba sluzba = new KnihaSluzba(); //instance třídy KnihaSluzba
+
+        System.out.println("Celkový počet knih " + sluzba.getKnihy().size());
+
+        System.out.println("Knihy Karel Čapek: " + sluzba.getKnihyOdZadanehoAutora("Karel Čapek"));
+
+        System.out.println("Knihy vydané roku 1845: ");
+        List<Kniha> knihy1845 = sluzba.getKnihyZRoku(1845);
+        for (Kniha kniha : knihy1845) {
+            System.out.println(knihy1845.get(0).getAutor() + ": " + kniha.getNazevKnihy());
+        }
 
     }
 
