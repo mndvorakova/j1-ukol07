95868cb95ab47d50a20daa235df0a86894787857
maja-nastassjadvorakova <dvorakova.maja12@gmail.com>
2025-06-22 20:41:10 +0200

    KnihaSluzba - objectMapper, načtení souboru

--- src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
+++ src/main/java/cz/czechitas/ukol07/KnihaSluzba.java
@@ -1,4 +1,26 @@
 package cz.czechitas.ukol07;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.List;
+
 public class KnihaSluzba {
+    private List<Kniha> knihy;
+
+
+    public KnihaSluzba(){
+        try(InputStream inputStream = KnihaSluzba.class.getResourceAsStream("knihy.json")) { //načte soubory z knihy.json
+            ObjectMapper objectMapper = new ObjectMapper();
+            knihy = objectMapper.readValue(inputStream, new TypeReference<List<Kniha>>() {});  //načtení knih
+
+        }
+    }
 }
